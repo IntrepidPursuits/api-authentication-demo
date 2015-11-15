53ffbfa98e9853ec6cc1dd775cc7bd6c46ca232a
Helen Hood <helen.c.hood@gmail.com>
2015-11-15 11:39:12 -0500

    Fix `v1/users#create` request spec

--- spec/requests/v1/users_requests_spec.rb
+++ spec/requests/v1/users_requests_spec.rb
@@ -4,8 +4,6 @@ describe 'Users endpoints' do
   describe 'POST /users' do
     context 'with valid email and password' do
       it 'returns JSON for a user' do
-        user = create(:user)
-
         user_attributes = {
           user: {
             email: user.email,
@@ -15,17 +13,18 @@ describe 'Users endpoints' do
 
         post(users_url, user_attributes, accept_headers)
 
-        user.reload
-
         expect(response).to have_http_status :ok
         expect_response_to_include_user_info
       end
     end
+
+  def user
+    @user ||= build(:user)
   end
 
   def expect_response_to_include_user_info
     expect(json_value_at_path('user/email')).to eq user.email
-    expect(json_value_at_path('user/authentication_token')).to eq user.authentication_token
+    expect(json_value_at_path('user/authentication_token')).to be
     expect(json_value_at_path('user/authentication_token_expires_at')).to be
   end
 end
